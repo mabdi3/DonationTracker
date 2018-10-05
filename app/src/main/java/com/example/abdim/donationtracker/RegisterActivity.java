e9855fc55af074239282e2b921ec699c75f84cf4
JWhite30515 <mrwhite@lawn-128-61-9-79.lawn.gatech.edu>
2018-10-04 23:46:25 -0400

    Added form validation errors, alert dialogs for incorrect input, enableCheck for register

--- app/src/main/java/com/example/abdim/donationtracker/RegisterActivity.java
+++ app/src/main/java/com/example/abdim/donationtracker/RegisterActivity.java
@@ -1,18 +1,20 @@
 package com.example.abdim.donationtracker;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
-
-
-import java.util.ArrayList;
+// import android.widget.TextView;
+// import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button back;
@@ -21,6 +23,18 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText password;
     private EditText confirmPassword;
     private Spinner accountType;
+    private final int USERNAME_MINLENGTH = 4;
+
+    /*
+     * Check to see if passwords match, if username filled out to enable register button
+     */
+    private void enableRegister() {
+
+        boolean isReady = username.getText().toString().length() >= USERNAME_MINLENGTH
+                && password.getText().toString().length() > 0
+                && confirmPassword.getText().toString().equals(password.getText().toString());
+        register.setEnabled(isReady);
+    };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,9 +50,65 @@ public class RegisterActivity extends AppCompatActivity {
         /*
           Set up the adapter to display the allowable AccountTypes in the spinner
          */
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, AccountType.values());
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, AccountType.values());
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         accountType.setAdapter(adapter);
+
+        enableRegister();
+
+        username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                if (!(username.getText().toString().length() >= USERNAME_MINLENGTH)) {
+                    username.setError("Username must be " + USERNAME_MINLENGTH +
+                            " characters or greater");
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                if (!(password.getText().toString().length() > 0)) {
+                    password.setError("Password must have at least one character");
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+        confirmPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                if (!(confirmPassword.getText().toString().equals(password.getText().toString()))) {
+                    confirmPassword.setError("Passwords must match");
+                }
+                enableRegister();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,16 +118,34 @@ public class RegisterActivity extends AppCompatActivity {
                 Account account = new Account(username.getText().toString(),
                         password.getText().toString(),
                         (AccountType) accountType.getSelectedItem());
-                if (password.getText().toString().equals(confirmPassword.getText().toString()) && !(rA.accountExists(account))) {
+                if (password.getText().toString().equals(confirmPassword.getText().toString())
+                        && !(rA.accountExists(account))) {
+
+
                     rA.addAccount(account);
+
+                    AlertDialog alertDialog = new AlertDialog.Builder(
+                            RegisterActivity.this).create();
+                    alertDialog.setTitle("Registration Successful");
+                    alertDialog.setMessage("User successfully added!");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    //send back to login page
+                                    Intent intent = new Intent(RegisterActivity.this,
+                                            MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            });
+                    alertDialog.show();
+
                 }
                 Log.d("Register",account.toString());
                 RegisteredAccounts.printData();
 
-                //send back to login page
-                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+
             }
         });
 
