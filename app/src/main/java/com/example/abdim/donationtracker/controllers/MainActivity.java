b7f82e5a51a6d9d4fb435d4e437c367e6b9e9b9e
Lwilson67gatech <claywilson99@att.net>
2018-10-13 21:52:08 -0400

    Patch Notes 10.13.18-A - Fixed the bug with the list not loading. My new activity was not in the manifest
    Patch Notes 10.13.18 - Clay
    - Added a page for viewing a list of locations
    - Added a corresponding activity
    - Added an activity for viewing a list
    - Once logged in, the button leads to the list of locations
    ! The program crashes upon trying to render the new page of locations. I have yet to see why. It seems to load the file just fine, I don't know what I missed. Help plz
    - Scar rate of fire reduced
    - Forgot to commit whole file
    
    Patch Notes 10.01.18 - Clay
    - Added some log messages for account creation and adding to the RegisteredAccounts
    - Fixed the crashing issue on the registration page! [It turns out that when making the Account object, if "User" wasn't selected, a String would be passed into the constructor instead of a Enum. This is what crashed the app]
    - Double Barrel damage reduced from 143/150 to 114/120.
    
    Patch Notes 9.30.18 - Clay
    - Added an Account class that stores user details
    - Added Register activity
    - Added a register button to activity_main.xml and functionality to MainActivity
    - Created a page for registering
    
    9.30.181 - Clay
    - Added 4 todos for the last bits of functionality
    - Cleaned up RegisterActivity
    - If you want to continue my work, start here!

--- app/src/main/java/com/example/abdim/donationtracker/controllers/MainActivity.java
+++ app/src/main/java/com/example/abdim/donationtracker/controllers/MainActivity.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -75,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         Info.setText("Login Attempts Remaining: 5");
-
         readSDFile();
     }
 
